3c0f3eedc2116369c183d5e25d30a1f106fa486c
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-16 14:46:08 +0100

    first commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class test {
+
+    //fixed size
+    static int simpleArray[]= new int[5];
+    public static void main(String[] args) {
+
+        for(int e : simpleArray){
+            System.out.println(e);
+        }
+
+    }
+}
