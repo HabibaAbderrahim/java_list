923c002fc5bf22523c9e9825ab84006e6a36461d
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-20 18:21:09 +0100

    Hashset / iterators

--- src/TestHashSet.java
+++ src/TestHashSet.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TestHashSet {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+//Difference btwn list and sets
+//Lists can contain duplicated value while sets contains unique elements
+public class TestHashSet {
+
+    static HashSet<Integer> mySet = new HashSet<>();
+    public static void main(String[] args) {
+        mySet.add(1);
+        mySet.add(1);//this value will not be shown since it's duplicated
+        mySet.add(0);
+        mySet.add(2);
+
+        Iterator<Integer> iterator = mySet.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
 }
--- src/TestIterator.java
+++ src/TestIterator.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class TestIterator {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+
+public class TestIterator {
+    /*
+    hasNext
+    next
+    previous
+    hasPrevious
+     */
+
+    static ArrayList<Integer> myList = new ArrayList<>();
+    public static void main(String[] args) {
+
+        //fill list
+        myList.add(1);
+        myList.add(10);
+        myList.add(11);
+        myList.add(110);
+        //iterator to myList
+        ListIterator<Integer> iterator = myList.listIterator();
+        //iteration
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("Iterate in reverse");
+        while (iterator.hasPrevious()){
+            System.out.println(iterator.previous());
+        }
+    }
 }
